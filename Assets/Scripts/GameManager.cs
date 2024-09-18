e93db087b489b8bc52934d68358e3b47ba7e633c
alfredvon <alfredvon126@hotmail.com>
2024-09-18 10:59:36 +0800

    去除rokid依赖（没用到）

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using Rokid.UXR;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/Grid/GridController.cs
+++ Assets/Scripts/Grid/GridController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem.HID;
 
 public class GridController : MonoBehaviour
 {
